94624f3b725a2fa5bdef90b770f904e239813f1c
TippingGame <tippinggame@163.com>
2024-11-13 02:27:29 +0800

    更新框架

--- Assets/F8Framework/Tests/Tween/DemoTween.cs
+++ Assets/F8Framework/Tests/Tween/DemoTween.cs
@@ -12,7 +12,8 @@ namespace F8Framework.Tests
 
         void Start()
         {
-            // 播放动画
+            /*--------------------------普通用法--------------------------*/
+            // 播放动画，设置Ease动画，设置OnComplete完成回调
             int id = gameObject.ScaleTween(Vector3.one, 1f).SetEase(Ease.Linear).SetOnComplete(OnViewOpen).ID;
 
             void OnViewOpen()
@@ -24,6 +25,7 @@ namespace F8Framework.Tests
             gameObject.RotateTween(Vector3.one, 1f);
             // 位移
             gameObject.Move(Vector3.one, 1f);
+            gameObject.MoveAtSpeed(Vector3.one, 2f);
             // 渐变
             gameObject.GetComponent<CanvasGroup>().Fade(0f, 1f);
             gameObject.GetComponent<Image>().ColorTween(Color.green, 1f);
@@ -34,17 +36,13 @@ namespace F8Framework.Tests
             gameObject.CancelTween(id);
             gameObject.CancelAllTweens();
 
-            // 根据相对坐标移动UI
-            // (0.0 , 1.0) _______________________(1.0 , 1.0)
-            //            |                      |
-            //            |                      |                  
-            //            |                      |
-            //            |                      |
-            // (0.0 , 0.0)|______________________|(1.0 , 0.0)
-            transform.GetComponent<RectTransform>().MoveUI(new Vector2(1f, 1f), canvasRect, 1f)
-                .SetEase(Ease.EaseOutBounce);
+            // 设置Delay
+            gameObject.Move(Vector3.one, 1.0f).SetDelay(2.0f);
             
-            // 你也可以这样使用
+            // 设置Event，在动画的某一时间调用
+            gameObject.Move(Vector3.one, 5.0f).SetEvent(OnViewOpen, 2.5f);
+            
+            // 你也可以这样使用，设置OnUpdate
             // 数字缓动变化
             BaseTween valueTween = FF8.Tween.ValueTween(0f, 100f, 3f).SetOnUpdateFloat((float v) =>
             {
@@ -60,6 +58,36 @@ namespace F8Framework.Tests
             });
             
             FF8.Tween.CancelTween(gameObjectTween.ID);
+            
+            // 根据相对坐标移动UI
+            // (0.0 , 1.0) _______________________(1.0 , 1.0)
+            //            |                      |
+            //            |                      |                  
+            //            |                      |
+            //            |                      |
+            // (0.0 , 0.0)|______________________|(1.0 , 0.0)
+            transform.GetComponent<RectTransform>().MoveUI(new Vector2(1f, 1f), canvasRect, 1f)
+                .SetEase(Ease.EaseOutBounce);
+            
+            /*--------------------------动画组合--------------------------*/
+            // 初始化，依次执行动画/并行执行动画，回调
+            var sequence = SequenceManager.GetSequence();
+            
+            sequence.Append(valueTween); // 第一个动画
+            sequence.Join(gameObjectTween);   // 与第一个动画同时执行
+            sequence.Append(valueTween); // 第二个动画完成后执行
+            sequence.Append(() => LogF8.Log("完成了！")); // 动画序列结束后的回调
+            sequence.SetOnComplete(() => LogF8.Log("Sequence 完成"));
+            
+            // 设置循环次数，-1代表无限循环
+            sequence.SetLoops(3);
+            
+            // 设置特定时间运行事件或动画
+            sequence.RunAtTime(() => LogF8.Log("半途事件"), 1.5f); // 在1.5秒时执行回调
+            sequence.RunAtTime(gameObjectTween, 2.0f); // 在2秒时开始执行特定动画
+            
+            // 回收Sequence，并停止所有动画
+            SequenceManager.KillSequence(sequence);
         }
     }
 }
