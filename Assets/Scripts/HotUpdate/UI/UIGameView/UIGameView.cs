99cf06691a0648ccffee6408a729476bab1dc943
TippingGame <tippinggame@163.com>
2025-01-04 15:59:04 +0800

    （13）下载管理器

--- Assets/Scripts/HotUpdate/UI/UIGameView/UIGameView.cs
+++ Assets/Scripts/HotUpdate/UI/UIGameView/UIGameView.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 using F8Framework.Core;
 using F8Framework.Launcher;
+using UnityEngine.Networking;
 
 public class UIGameView : BaseView
 {
@@ -54,10 +56,109 @@ public class UIGameView : BaseView
         });
     }
     
+    private string[] fileInfos = new[]
+    {
+        "https://raw.githubusercontent.com/TippingGame/F8Framework/refs/heads/main/Tests/Logo.png"
+    };
+    
+    private Downloader downloader;
+    
     // Start
     protected override void OnStart()
     {
         
+        // 创建下载器
+        downloader = FF8.Download.CreateDownloader("Download", new Downloader());
+        
+        // 设置超时时间，默认为无超时时间
+        downloader.DownloadTimeout = 30;
+            
+        // 设置下载器回调
+        downloader.OnDownloadSuccess += OnDownloadSucess;
+        downloader.OnDownloadFailure += OnDownloadFailure;
+        downloader.OnDownloadStart += OnDownloadStart;
+        downloader.OnDownloadOverallProgress += OnDownloadOverall;
+        downloader.OnAllDownloadTaskCompleted += OnDownloadFinish;
+            
+        int count = 0;
+        // 添加下载清单
+        foreach (var fileInfo in fileInfos)
+        {
+            count += 1;
+            
+            string downloadPath = Application.dataPath + "/Logo_" + count + ".png";
+            long fileSizeInBytes = 0;
+            
+            // 断点续传
+            if (File.Exists(downloadPath))
+            {
+                FileInfo downloadFileInfo = new FileInfo(downloadPath);
+                fileSizeInBytes = downloadFileInfo.Length;
+            }
+            
+            downloader.AddDownload(fileInfo, downloadPath, fileSizeInBytes, true);
+        }
+            
+        // 下载器开始下载
+        downloader.LaunchDownload();
+    }
+    
+    // 开始下载
+    void OnDownloadStart(DownloadStartEventArgs eventArgs)
+    {
+        LogF8.Log(eventArgs.DownloadInfo.DownloadUrl);
+    }
+        
+    void OnDownloadOverall(DonwloadUpdateEventArgs eventArgs)
+    {
+        LogF8.Log(eventArgs.DownloadInfo.DownloadProgress);
+        // 测试断点续传，暂停下载
+        // if (eventArgs.DownloadInfo.DownloadProgress > 0.4f)
+        // {
+        //     downloader.CancelDownload();
+        //     return;
+        // }
+        
+        // 部分下载任务本身仅知道下载连接，无法获取需要下载的二进制长度，无法使用更精准的进度。
+        float currentTaskIndex = (float)eventArgs.CurrentDownloadTaskIndex;
+        float taskCount = (float)eventArgs.DownloadTaskCount;
+
+        // 计算进度百分比
+        float progress = currentTaskIndex / taskCount * 100f;
+        // LogF8.Log(progress);
+    }
+        
+    // 下载成功
+    void OnDownloadSucess(DownloadSuccessEventArgs eventArgs)
+    {
+        LogF8.Log($"DownloadSuccess {eventArgs.DownloadInfo.DownloadUrl}");
+    }
+        
+    // 下载失败
+    void OnDownloadFailure(DownloadFailureEventArgs eventArgs)
+    {
+        LogF8.LogError($"DownloadFailure {eventArgs.DownloadInfo.DownloadUrl}\n{eventArgs.ErrorMessage}");
+    }
+        
+    // 所有下载完成
+    void OnDownloadFinish(DownloadTasksCompletedEventArgs eventArgs)
+    {
+        LogF8.Log($"DownloadFinish {eventArgs.TimeSpan}");
+        
+        string downloadPath = Application.dataPath + "/Logo_1.png";
+        StartCoroutine(DownloadImage(downloadPath));
+    }
+    
+    IEnumerator DownloadImage(string downloadPath)
+    {
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(downloadPath);
+        yield return request.SendWebRequest();
+        if (request.result == UnityWebRequest.Result.ProtocolError || request.result == UnityWebRequest.Result.ConnectionError)
+            LogF8.LogError(request.error);
+        else
+        {
+            rimg_logo_rimg.texture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+        }
     }
     
     protected override void OnViewTweenInit()
@@ -92,6 +193,9 @@ public class UIGameView : BaseView
     // 自动获取组件（自动生成，不能删除）
     [SerializeField] private Transform tf_group_tf;
     [SerializeField] private Text TextLegacy_TextLegacy;
+    [SerializeField] private Text TextLegacy1_TextLegacy;
+    [SerializeField] private RawImage rimg_logo_rimg;
+    [SerializeField] private GameObject rimg_logo_go;
     [SerializeField] private Transform tf_hp_tf;
 
 #if UNITY_EDITOR
@@ -99,6 +203,9 @@ public class UIGameView : BaseView
     {
         tf_group_tf = transform.Find("tf_group").GetComponent<Transform>();
         TextLegacy_TextLegacy = transform.Find("Text (Legacy)").GetComponent<Text>();
+        TextLegacy1_TextLegacy = transform.Find("Text (Legacy) (1)").GetComponent<Text>();
+        rimg_logo_rimg = transform.Find("rimg_logo").GetComponent<RawImage>();
+        rimg_logo_go = transform.Find("rimg_logo").gameObject;
         tf_hp_tf = transform.Find("tf_group/tf_hp").GetComponent<Transform>();
     }
 #endif
