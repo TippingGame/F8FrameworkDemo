7cfae352fb9adf10b2e2ee6025ef906b71bd3959
TippingGame <tippinggame@163.com>
2025-11-14 21:29:52 +0800

    更新框架

--- Assets/F8Framework/ConfigData/F8DataManager/F8DataManager.cs
+++ Assets/F8Framework/ConfigData/F8DataManager/F8DataManager.cs
@@ -7,9 +7,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
 using F8Framework.Core;
 using UnityEngine.Scripting;
 
